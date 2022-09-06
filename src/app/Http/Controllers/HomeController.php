950f00c76d47ae16ed26f9158d77d96ee9ec0160
Toy Rik <toyrik@gmail.com>
2022-09-06 08:16:23 +0300

    Оптимизация запроса в контроллере.

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $articles = Article::orderBy('created_at', 'desc')->get()->take(6);
+        $articles = Article::with('state', 'tags')->orderBy('created_at', 'desc')->limit(6)->get();
         return view('app.home', compact('articles'));
     }
 }
